7dc251bec67c87f52e836623458cb1880813ed13
thesachinjat <57931486+thesachinjat@users.noreply.github.com>
2021-10-27 16:55:13 +0530

    Update Something.py

--- Something.py
+++ Something.py
@@ -1,15 +1,25 @@
-def swapList(newList):
-    size = len(newList)
-     
-    # Swapping
-    temp = newList[0]
-    newList[0] = newList[size - 1]
-    newList[size - 1] = temp
-   
-    print("result: ")
-    return newList
-     
-# Driver code
-newList = [12, 35, 9, 56, 24]
- 
-print(swapList(newList))
+# Program to multiply two matrices using nested loops
+
+# 3x3 matrix
+X = [[12,7,3],
+    [4 ,5,6],
+    [7 ,8,9]]
+# 3x4 matrix
+Y = [[5,8,1,2],
+    [6,7,3,0],
+    [4,5,9,1]]
+# result is 3x4
+result = [[0,0,0,0],
+         [0,0,0,0],
+         [0,0,0,0]]
+
+# iterate through rows of X
+for i in range(len(X)):
+   # iterate through columns of Y
+   for j in range(len(Y[0])):
+       # iterate through rows of Y
+       for k in range(len(Y)):
+           result[i][j] += X[i][k] * Y[k][j]
+
+for r in result:
+   print(r)
